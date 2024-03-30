01ccda32ce8c16792f0505c5d8713d12eed4ae09
Memittal13 <163784314+Memittal13@users.noreply.github.com>
2024-03-30 16:46:56 -0700

    Update Exercise_1.cpp

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,11 +1,26 @@
-#include <stdio.h> 
-  
+//
+//TIME Complexity: O(log n)
+//SPACE Complexity: O(n)
 // A recursive binary search function. It returns 
 // location of x in given array arr[l..r] is present, 
 // otherwise -1 
+#include <stdio.h> 
 int binarySearch(int arr[], int l, int r, int x) 
 {   
-    //Your Code here 
+   
+   int mid = 0;
+    
+   while(l<=r) {
+        mid = l + (r-l)/2;
+        if(arr[mid] == x) {
+            return mid;
+        } else if(x < arr[mid]) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return -1;
 } 
   
 int main(void) 
@@ -14,8 +29,19 @@ int main(void)
     int n = sizeof(arr) / sizeof(arr[0]); 
     int x = 10; 
     int result = binarySearch(arr, 0, n - 1, x); 
-    (result == -1) ? printf("Element is not present in array") 
-                   : printf("Element is present at index %d", 
+    (result == -1) ? printf("Element is not present in array \n") 
+                   : printf("Element is present at index %d \n", 
                             result); 
+    x = 5; 
+    result = binarySearch(arr, 0, n - 1, x); 
+    (result == -1) ? printf("Element is not present in array \n") 
+                   : printf("Element is present at index %d \n", 
+                            result);
+    x = 40; 
+    result = binarySearch(arr, 0, n - 1, x); 
+    (result == -1) ? printf("Element is not present in array \n") 
+                   : printf("Element is present at index %d \n", 
+                            result); 
+    
     return 0; 
 } 
